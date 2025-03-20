58f3bfbb0552064dcac5324dfe9295fafdd22041
610weblabdev <access.610weblab@gmail.com>
2025-03-20 18:08:40 +0530

    fix ananlitcs

--- app/routes/pages.$handle.jsx
+++ app/routes/pages.$handle.jsx
@@ -55,16 +55,8 @@ async function addToCartAndCheckout(productId, variantId) {
       checkoutUrl += (checkoutUrl.includes('?') ? '&' : '?') + currentParams.substring(1);
     }
 console.log(checkoutUrl);
-
-  alert("analytics");
-  window.ShopifyAnalytics.lib.track('add_to_cart', {
-    currency: 'USD',
-    value: 29.99, // Adjust as needed
-    product_id: $variantId,
-    quantity: 1
-  });
-
-
+  
+  
     // Redirect to checkout
     window.location.href = checkoutUrl;
   } catch (error) {
