e54a53752256c66c0161244597c2cf1f8ebe35ea
610weblabdev <access.610weblab@gmail.com>
2025-03-20 16:56:21 +0530

    add analytics

--- app/routes/pages.$handle.jsx
+++ app/routes/pages.$handle.jsx
@@ -55,6 +55,17 @@ async function addToCartAndCheckout(productId, variantId) {
       checkoutUrl += (checkoutUrl.includes('?') ? '&' : '?') + currentParams.substring(1);
     }
 console.log(checkoutUrl);
+
+if (window.ShopifyAnalytics && window.ShopifyAnalytics.lib) {
+  alert("analytics");
+  window.ShopifyAnalytics.lib.track('add_to_cart', {
+    currency: 'USD',
+    value: 29.99, // Adjust as needed
+    product_id: $variantId,
+    quantity: 1
+  });
+}
+
     // Redirect to checkout
     window.location.href = checkoutUrl;
   } catch (error) {
